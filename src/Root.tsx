e0d8cff87638d8f5a0a9d60078d049f1ff567dc1
Monir Saikat <118444503+MonirSaikat@users.noreply.github.com>
2024-07-12 10:46:26 +0600

    index home

--- src/Root.tsx
+++ src/Root.tsx
@@ -3,6 +3,7 @@ import { Outlet } from "react-router-dom";
 function Root() {
   return (
     <>
+      <h1>Root</h1>
       <Outlet />
     </>
   );
--- src/routes.tsx
+++ src/routes.tsx
@@ -2,12 +2,17 @@ import { createBrowserRouter } from "react-router-dom";
 import Root from "./Root";
 import { Site } from "./pages/Site";
 import { App } from "./pages/App";
+import { Home } from "./pages/Home";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
     children: [
+      {
+        index: true,
+        element: <Home />, 
+      },
       {
         path: "site",
         element: <Site />,
