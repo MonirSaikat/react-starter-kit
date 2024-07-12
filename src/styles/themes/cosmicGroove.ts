6253b7eb93a4879714411b5fa7fdb07150e29a68
Monir Saikat <118444503+MonirSaikat@users.noreply.github.com>
2024-07-12 12:22:26 +0600

    type hinting

---
+++ src/styles/themes/cosmicGroove.ts
@@ -0,0 +1,10 @@
+import { Theme } from "@emotion/react";
+
+export const cosmicGroove: Theme = {
+  color: {
+    primary: "#6A0D91",
+    secondary: "#F14D54",
+    light: "#F4E1D2",
+    dark: "#001F3F",
+  },
+};
---
+++ src/styles/themes/neonDreamscape.ts
@@ -0,0 +1,10 @@
+import { Theme } from "@emotion/react";
+
+export const neonDreamscape: Theme = {
+  color: {
+    primary: "#39FF14",
+    secondary: "#FF007F",
+    light: "#D4FFFE",
+    dark: "#2F2F2F",
+  },
+};
