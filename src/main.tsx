a930c21c61a8167c597e9386646ea758e86e13d1
Monir Saikat <118444503+MonirSaikat@users.noreply.github.com>
2024-07-12 12:54:18 +0600

    absolute path

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import GlobalStyles from "./styles/GlobalStyles.tsx";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes.tsx";
+import GlobalStyles from "@styles/GlobalStyles.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
