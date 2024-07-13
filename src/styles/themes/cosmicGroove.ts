1a2049098e66f4efe14883024c891ee344c8e508
Monir Saikat <118444503+MonirSaikat@users.noreply.github.com>
2024-07-14 00:29:48 +0600

    primary color changed

--- src/styles/themes/cosmicGroove.ts
+++ src/styles/themes/cosmicGroove.ts
@@ -2,7 +2,7 @@ import { Theme } from "@emotion/react";
 
 export const cosmicGroove: Theme = {
   color: {
-    primary: "#6A0D91",
+    primary: "#001F3F",
     secondary: "#F14D54",
     light: "#F4E1D2",
     dark: "#001F3F",
