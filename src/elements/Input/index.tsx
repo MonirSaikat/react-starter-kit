b8cdc03882a5ac9b7e6c56b9b7769044c67bb5d0
Monir Saikat <118444503+MonirSaikat@users.noreply.github.com>
2024-07-14 22:08:57 +0600

    props handle

--- src/elements/Input/index.tsx
+++ src/elements/Input/index.tsx
@@ -1,9 +1,11 @@
 import React, { ReactNode } from "react";
 import { StyledInput, StyledInputContainer } from "./Input.styled";
 
-interface IInput extends Partial<React.ComponentProps<'input'>> {
+interface IInput extends Omit<Partial<React.ComponentProps<'input'>>, 'size'> {
   icon?: ReactNode;
   iconPosition?: 'left' | 'right';
+  size?: 'lg' | 'sm' | undefined;
+  value?: string | undefined | string[];
 }
 
 export const Input: React.FC<IInput> = ({ icon, iconPosition = 'right', ...otherProps }) => {
